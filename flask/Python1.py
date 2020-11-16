cfbd972f14cfbd11556cca04f3f5f5ac15596bb0
Palak-137 <kumaripalak.aps@gmail.com>
2020-11-16 14:49:32 +0530

    ppt converted

--- flask/Python1.py
+++
@@ -1,7 +0,0 @@
-# importing required libraries
-import pandas as pd
-from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS, TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.pipeline import Pipeline
-from sklearn.metrics import f1_score
-from sklearn.model_selection import train_test_split
